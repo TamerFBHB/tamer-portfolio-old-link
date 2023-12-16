31e1d6e16d65a86f8a812dfa5edc152f1bd0f5c3
Tamer Fouad <tamer.fbhb.wd@gmail.com>
2023-12-16 20:12:11 +0200

    edit in contact

--- src/component/ContactForm/ContactForm.jsx
+++ src/component/ContactForm/ContactForm.jsx
@@ -6,48 +6,41 @@ import { Container } from "react-bootstrap";
 const ContactForm = () => {
   // **** apear alert when send mail****
   const  Alert =()=> {
-    alert("Your message is send")
+    alert("your message is send")
 }
 // **** this code to send message when click send input ****
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs
-      .sendForm(
-        "service_ud5dkct",
-        "template_lzvvg4f",
-        form.current,
-        "iWyQCE4knWLge4c2A"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
+      .sendForm( "service_ud5dkct", "template_lzvvg4f",form.current, "iWyQCE4knWLge4c2A" )
+      .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
     Alert();
-  };
+};
+    
+
   return (
     <>
       <div className="sendEmail">
         <h3>Send Me Email</h3>
         
       <Container>
-          <form ref={form} onSubmit={sendEmail}>
+          <form ref={form} onSubmit={sendEmail} >
             <div>
-              <label>Your Name</label>
-              <input type="text" name="user_name" />
+              <label id="name">Your Name</label>
+              <input type="text" name="user_name"  id="tamer" required />
             </div>
             <div>
               <label>Your Email</label>
-              <input type="email" name="user_email" />
+              <input type="email" name="user_email" id="mail" required/>
             </div>
             <div>
               <label>Message</label>
-              <textarea name="message" cols="45" rows="5" />
+              <textarea name="message" cols="45" rows="5" id="message" required/>
             </div>
           <div className="btnSend">
               <div className="send "  >
